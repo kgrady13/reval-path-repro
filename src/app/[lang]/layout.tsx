3376f766b28ad0e8d516c26bf0b42fb954e353f8
nicdillon <nicholas.dillon@vercel.com>
2026-01-14 14:48:51 -0500

    Disable generateStaticParams to test on-demand revalidation
    Pages prerendered with generateStaticParams cannot be reliably
    revalidated on Vercel using revalidatePath (known issue).
    
    Testing if removing static pregeneration fixes on-demand ISR.
    
    See: https://github.com/vercel/next.js/issues/59883

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,8 @@
-export function generateStaticParams() {
-  return [{ lang: "en" }, { lang: "es" }];
-}
+// Temporarily disabled to test if generateStaticParams prevents on-demand revalidation
+// See: https://github.com/vercel/next.js/issues/59883
+// export function generateStaticParams() {
+//   return [{ lang: "en" }, { lang: "es" }];
+// }
 
 export default function LangLayout({
   children,
